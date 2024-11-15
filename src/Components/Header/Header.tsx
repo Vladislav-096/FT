156d57dd9f8726d8245b927b30ac3682d95905bc
VladislavVanyushkin <vlad.vanyushkin.96@mail.ru>
2024-11-15 17:38:26 +0300

    feat: added styles for a few elements

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,29 +5,43 @@ import bell from "../../assets/bell.svg";
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="container">
-        <picture>
-          <img src={logo} alt="Logo" />
-        </picture>
-        <p className={styles.location}>Москва и область</p>
-        <picture>
-          <img src={bell} alt="Bell icon" />
-        </picture>
-        <div className={styles["burger-menu"]}>
-          <div className={styles["burger-bar"]}></div>
-          <div className={styles["burger-bar"]}></div>
-          <div className={styles["burger-bar"]}></div>
+      <div className={styles["header-upper"]}>
+        <div className="container">
+          <div className={styles["header-upper-content"]}>
+            <picture className={styles["logo-picture"]}>
+              <img className={styles["logo-img"]} src={logo} alt="Logo" />
+            </picture>
+            <p className={styles.location}>Москва и область</p>
+            <picture className={styles["bell-picture"]}>
+              <img className={styles["bell-img"]} src={bell} alt="Bell icon" />
+            </picture>
+            <div className={styles["burger-menu"]}>
+              <div className={styles["burger-bar"]}></div>
+              <div className={styles["burger-bar"]}></div>
+              <div className={styles["burger-bar"]}></div>
+            </div>
+          </div>
         </div>
-        <nav>
-          <ul>
-            <li>Преимущества Tele2</li>
-            <li>Тарифы</li>
-            <li>Акции и спецпредложения</li>
-            <li>Промотариф Tele2</li>
-            <li>Технология eSIM</li>
-            <li>Подключение нового абонента</li>
-          </ul>
-        </nav>
+      </div>
+      <div className={styles["header-lower"]}>
+        <div className="container">
+          <div className={styles["header-lower-content"]}>
+            <nav className={styles.nav}>
+              <ul className={`list-reset ${styles["nav-lsit"]}`}>
+                <li className={styles["nav-list-item"]}>Преимущества Tele2</li>
+                <li className={styles["nav-list-item"]}>Тарифы</li>
+                <li className={styles["nav-list-item"]}>
+                  Акции и спецпредложения
+                </li>
+                <li className={styles["nav-list-item"]}>Промотариф Tele2</li>
+                <li className={styles["nav-list-item"]}>Технология eSIM</li>
+                <li className={styles["nav-list-item"]}>
+                  Подключение нового абонента
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </header>
   );
